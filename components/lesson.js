0f25109b7b8a6e978a1ed82b3feddfa70695046b
achira-sketch <asdstdf@gmail.com>
2023-03-11 12:57:21 +0200

    ylika easter egg, favicon

--- components/lesson.js
+++ components/lesson.js
@@ -1,19 +1,28 @@
 import styles from "styles/First.module.css";
 import React, { useState } from "react";
 
-export default function Lesson({ data , onClick }) {
+export default function Lesson({ data, onClick }) {
   const [selected, setSelected] = useState(data.selected);
   const callback = () => {
     setSelected(!data.selected);
     data.selected = !data.selected;
     onClick(!data.selected);
-
   };
 
   return (
-    <div onClick={callback} className={data.selected ? styles.box2 : styles.box}>
-        <div>{data.type == "Υποχρεωτικό (Υ)" && (<span className={styles.mandatory}>! </span>)}{data.name}</div> 
-        <div>{data.ects}</div>
+    <div
+      onClick={callback}
+      className={`${data.selected ? styles.box2 : styles.box} 
+      ${data.code == "4356" && !data.selected && styles.ylika} 
+      `}
+    >
+      <div>
+        {data.type == "Υποχρεωτικό (Υ)" && (
+          <span className={styles.mandatory}>! </span>
+        )}
+        {data.name}
+      </div>
+      <div>{data.ects}</div>
     </div>
   );
 }
